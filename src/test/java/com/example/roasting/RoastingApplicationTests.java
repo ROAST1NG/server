1d08869691d16dd7d3cee3b0a84ae5972b02b2bf
Ubinquitous <ubinquitous1@gmail.com>
2023-08-22 10:34:33 +0900

    init

---
+++ src/test/java/com/example/roasting/RoastingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roasting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoastingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
